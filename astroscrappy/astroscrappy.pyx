21a04c61dcf43dd2fe00c4775a97caa4ea93f6a7
Jonatan Selsing <jselsing@dark-cosmology.dk>
2016-05-19 13:02:56 +0200

    Replacing negative laplacians with abs

--- astroscrappy/astroscrappy.pyx
+++ astroscrappy/astroscrappy.pyx
@@ -244,8 +244,7 @@ def detect_cosmics(indat, inmask=None, float sigclip=4.5, float sigfrac=0.3,
         del subsam
 
         # Replacing negative values by large number
-        print(conved)
-        conved[conved < 0] = max(conved)
+        conved[conved < 0] = abs(conved)
         # This is called L+ in the original LA Cosmic/cosmics.py
         s = rebin(conved)
         del conved
